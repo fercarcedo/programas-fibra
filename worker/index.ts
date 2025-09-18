460cf1558f2c0dbe9787e75bbc046a3ea26d5f1c
Fernando García Álvarez <fercarcedo@gmail.com>
2025-09-18 23:47:37 +0200

    feat: expose aggregated data through worker endpoint

--- worker/index.ts
+++ worker/index.ts
@@ -1,6 +1,31 @@
-export default {
-  fetch(request) {
+import { WorkerEntrypoint } from "cloudflare:workers";
+
+interface Env {
+  BUCKET_GEO: R2Bucket;
+}
+
+export default class extends WorkerEntrypoint<Env> {
+  async fetch(request: Request) {
     const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/data/")) {
+      const key = url.pathname.replace(/^\/data\//, "");
+
+      if (key.startsWith("aggregated-")) {
+        const object = await this.env.BUCKET_GEO.get(key);
+
+        if (!object) {
+          return new Response("Not found", { status: 404 });
+        }
+        
+        return new Response(object.body, {
+          headers: {
+            "content-type": "application/json; charset=UTF-8",
+            "cache-control": "public, max-age=31536000, immutable",
+          },
+        });
+      }
+    }
 
     if (url.pathname.startsWith("/api/")) {
       return Response.json({
@@ -8,5 +33,5 @@ export default {
       });
     }
     return new Response(null, { status: 404 });
-  },
-} satisfies ExportedHandler<Env>;
+  }
+}
