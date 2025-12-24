a7e68377fe2b5f89bbd2674604dd332aaf240f1e
Fernando García Álvarez <fercarcedo@gmail.com>
2025-12-24 14:43:22 +0100

    feat: updated process files script

---
+++ programas-fibra-data-processing/process_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#ogr2ogr -f GeoJSON -t_srs EPSG:4326 ZonasElegiblesFinales2024.geojson '/home/fer/Ficheros_programas_fibra/UNICO_2024/ZonasElegiblesFinales2024.shp'
+#uv run main.py unico -e ZonasElegiblesFinales2024.geojson -a '/home/fer/Ficheros_programas_fibra/Relacion_proyectos_aprobados_UNICO_Banda_Ancha_2024_19_12_2025.xlsx' -p "UNICO 2024"
+#tippecanoe -z18 --projection=EPSG:4326 -o unico2024.pmtiles -l output out.geojson --force
+#tile-join -o merged.pmtiles unico2024.pmtiles --force
+#ogr2ogr -f GeoJSON -t_srs EPSG:4326 ZonasElegibles2023.geojson '/home/fer/Ficheros_programas_fibra/UNICO_2023/ZonasElegibles2023.shp'
+#uv run main.py unico -e ZonasElegibles2023.geojson -a '/home/fer/Ficheros_programas_fibra/Relacion_proyectos_aprobados_UNICO_Banda_Ancha_2023_19_12_2025.xlsx' -p "UNICO 2023"
+#tippecanoe -z18 --projection=EPSG:4326 -o unico2023.pmtiles -l output out.geojson --force
+#ogr2ogr -f GeoJSON -t_srs EPSG:4326 ZonasElegibles2022_UNICO_BA.geojson '/home/fer/Ficheros_programas_fibra/UNICO_2022/ZonasElegibles2022_UNICO_BA.shp'
+uv run main.py unico -e ZonasElegibles2022_UNICO_BA.geojson -a '/home/fer/Ficheros_programas_fibra/Relacion_proyectos_aprobados_UNICO_Banda_Ancha_2022_19_12_2025.xlsx' -p "UNICO 2022"
+#tippecanoe -z18 --projection=EPSG:4326 -o unico2024.pmtiles -l output out.geojson --force
